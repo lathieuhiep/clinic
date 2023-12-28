7c47ebb00cc6e978ca9869c73e3af6b030bb56c3
lathieuhiep <khacdiepkma90@gmail.com>
2023-12-28 15:29:10 +0700

    fix dropdown category

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,76 +12,33 @@
 
     $( document ).ready( function () {
 
-        /* Start back top */
+        // handle click back to top
         $('#back-top').on( 'click', function (e) {
-            e.preventDefault();
-            $('html').scrollTop(0);
-        } );
-        /* End back top */
+            e.preventDefault()
+            $('html').scrollTop(0)
+        } )
 
-        /* btn mobile Start*/
-        let subMenuToggle  =   $('.sub-menu-toggle');
+        // handle click show submenu on mobile
+        handleClickShowSubmenuOnMobile()
 
-        if ( subMenuToggle.length ) {
+        // handle dropdown category widget
+        handleDropdownCategoryWidget()
 
-            subMenuToggle.each(function () {
-                $(this).on( 'click', function () {
-                    const widthScreen = $(window).width();
-
-                    if ( widthScreen < 992 ) {
-                        $(this).toggleClass('active');
-                        $(this).closest( '.menu-item-has-children' ).siblings().find( subMenuToggle ).removeClass( 'active' );
-                        $(this).parent().children( '.sub-menu' ).slideToggle();
-                        $(this).parents( '.menu-item-has-children' ).siblings().find( '.sub-menu' ).slideUp();
-                    }
-
-                } )
-            })
-
-        }
-        /* btn mobile End */
-
-        // dropdown category widget
-        const cateLinkHasChildWidget = $('.categories-dropdown-widget .cate-link-has-child')
-
-        if ( cateLinkHasChildWidget.length ) {
-            cateLinkHasChildWidget.each(function () {
-                $(this).on('click', function (e) {
-                    e.preventDefault()
-
-                    $(this).toggleClass('active')
-                    $(this).closest( '.cat-item' ).siblings().find(cateLinkHasChildWidget).removeClass( 'active' )
-                    $(this).parent().children( '.children' ).slideToggle();
-                    $(this).parents( '.cat-item-has-child' ).siblings().find('.children').slideUp();
-                })
-            })
-        }
-
-        //
-        const singlePostContentDetail = $('.single-post-content__detail')
-
-        if ( singlePostContentDetail.length ) {
-            const urlImageCate = singlePostContentDetail.data('url-image-cate')
-
-            // insert before image cate
-            $(`<div class="image-cate-box text-center mb-4"><img src="${urlImageCate}" alt=""></div>`).insertBefore(".title-has-icon:first");
-        }
-    });
+        // handle dropdown category widget
+        handleAddCategoryImageContentPost()
+    })
 
     // loading
     $( window ).on( "load", function() {
-
-        $( '#site-loadding' ).remove()
-
-    });
+        // handle remove loading page after loaded successfully
+        handleRemoveLoadingPage()
+    })
 
     // scroll event
     $( window ).scroll( function() {
-
         if ( timer_clear ) clearTimeout(timer_clear)
 
         timer_clear = setTimeout( function() {
-
             /* Start scroll back top */
             const $scrollTop = $(this).scrollTop();
 
@@ -106,9 +63,86 @@
                     topNavMobile.find('.item .hotline-on-mobile').removeClass('scroll-show-phone')
                 }
             }
-
         }, 100 );
 
-    });
+    })
+
+    /*
+    ** Function
+    * */
+
+    // handle remove loading page after loaded successfully
+    const handleRemoveLoadingPage = () => {
+        const siteLoading = $( '#site-loading' )
+
+        if ( siteLoading.length ) {
+            siteLoading.remove()
+        }
+    }
+
+    // handle click show submenu on mobile
+    const handleClickShowSubmenuOnMobile = () => {
+        const subMenuToggle = $('.sub-menu-toggle')
+
+        if ( subMenuToggle.length ) {
+            subMenuToggle.each(function () {
+                $(this).on( 'click', function () {
+                    const widthScreen = $(window).width()
+
+                    if ( widthScreen < 992 ) {
+                        $(this).toggleClass('active')
+                        $(this).closest( '.menu-item-has-children' ).siblings().find( subMenuToggle ).removeClass( 'active' )
+                        $(this).parent().children( '.sub-menu' ).slideToggle()
+                        $(this).parents( '.menu-item-has-children' ).siblings().find( '.sub-menu' ).slideUp()
+                    }
+                } )
+            })
+        }
+    }
+
+    // handle dropdown category widget
+    const handleDropdownCategoryWidget = () => {
+        // handle show cate current
+        const cateItemLink = $('.categories-dropdown-widget .cat-item__link')
+        if ( cateItemLink.length ) {
+            cateItemLink.each(function () {
+                const hasClassCurrent = $(this).hasClass('current-cate')
+
+                if ( hasClassCurrent ) {
+                    const catItemParent = $(this).closest('.cat-item-parent')
+
+                    catItemParent.children('.cate-link-has-child').addClass('active')
+                    catItemParent.children( '.children' ).slideDown()
+                }
+            })
+        }
+
+        // handle slideToggle
+        const cateLinkHasChildWidget = $('.categories-dropdown-widget .cate-link-has-child')
+        if ( cateLinkHasChildWidget.length ) {
+            cateLinkHasChildWidget.each(function () {
+                $(this).on('click', function (e) {
+                    e.preventDefault()
+
+                    $(this).toggleClass('active')
+                    $(this).closest( '.cat-item' ).siblings().find(cateLinkHasChildWidget).removeClass( 'active' )
+                    $(this).parent().children( '.children' ).slideToggle()
+                    $(this).parents( '.cat-item-has-child' ).siblings().find('.children').slideUp();
+                })
+            })
+        }
+    }
+
+    // handle add category image content post
+    const handleAddCategoryImageContentPost = () => {
+        const singlePostContentDetail = $('.single-post-content__detail')
+
+        if ( singlePostContentDetail.length ) {
+            const urlImageCate = singlePostContentDetail.data('url-image-cate')
+
+            // insert before image cate
+            $(`<div class="image-cate-box text-center mb-4"><img src="${urlImageCate}" alt=""></div>`).insertBefore(".title-has-icon:first");
+        }
+    }
 
 } )( jQuery );
--- components/inc-loading.php
+++ components/inc-loading.php
@@ -4,7 +4,7 @@ $show_loading = clinic_get_option( 'opt_general_loading', '0' );
 if(  $show_loading == '1' ) :
     $opt_image_loading  = clinic_get_option( 'opt_general_image_loading' );
 ?>
-    <div id="site-loadding" class="d-flex align-items-center justify-content-center">
+    <div id="site-loading" class="d-flex align-items-center justify-content-center">
         <?php if ( !empty( $opt_image_loading['url'] ) ): ?>
             <img class="loading_img" src="<?php echo esc_url( $opt_image_loading['url'] ); ?>" alt="<?php esc_attr_e('loading...','clinic') ?>"  >
         <?php else: ?>
