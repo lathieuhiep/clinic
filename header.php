173e931253599c89345f117380eb77116c4f56e2
lathieuhiep <khacdiepkma90@gmail.com>
2024-01-05 11:34:38 +0700

    remove google

--- header.php
+++ header.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VMR49GWBBM"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-VMR49GWBBM');
-    </script>
-
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,12 +8,6 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5K456WXT" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
-
 <?php
 if ( !is_404() ) :
 	get_template_part('components/inc','header');
@@ -32,5 +16,3 @@ endif;
 
 <!--Start Sticky Footer-->
 <div class="sticky-footer">
-
-
