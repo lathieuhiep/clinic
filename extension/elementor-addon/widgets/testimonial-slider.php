0d756b238551e3376c96b7349e1097f228742d41
lathieuhiep <khacdiepkma90@gmail.com>
2024-01-08 17:22:26 +0700

    update core

--- extension/elementor-addon/widgets/testimonial-slider.php
+++ extension/elementor-addon/widgets/testimonial-slider.php
@@ -8,7 +8,7 @@ use Elementor\Controls_Manager;
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class clinic_Elementor_Addon_Testimonial_Slider extends Widget_Base {
+class Clinic_Elementor_Addon_Testimonial_Slider extends Widget_Base {
     public function get_categories(): array {
         return array( 'my-theme' );
     }
