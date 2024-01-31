ae004de68cf266e177a22aa37cf82ee799b39065
lathieuhiep <khacdiepkma90@gmail.com>
2024-01-31 15:25:00 +0700

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ const pathNodeModule = './node_modules'
 // server
 function server() {
     browserSync.init({
-        proxy: "localhost/phukhoa199",
+        proxy: "localhost/phukhoaquoctedanang",
         port: 3000,
         open: false,
         cors: true,
