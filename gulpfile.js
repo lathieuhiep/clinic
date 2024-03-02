8b675bcc093dd48c2bbe332cb16de1a0f19f3505
lathieuhiep <khacdiepkma90@gmail.com>
2024-03-02 14:12:33 +0700

    update code

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ const pathNodeModule = './node_modules'
 // server
 function server() {
     browserSync.init({
-        proxy: "localhost/dakhoadanang180tranphu",
+        proxy: "localhost/namkhoaquoctedanang",
         open: false,
         cors: true,
         ghostMode: false
